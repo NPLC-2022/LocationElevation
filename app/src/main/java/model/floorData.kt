fe9678a3d1c02dfd439a256e19f50ea83a06258b
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2022-09-29 07:20:09 +0700

    Base App
    I can track real time elevation changes. Use the Client Provider, the one without will be discontinued soon

---
+++ app/src/main/java/model/floorData.kt
@@ -0,0 +1,8 @@
+package model
+
+/*
+    name - Well, the name of the floor - 5th or 6th or 7th
+    height - This is the minimum amount set to determine the altitude.
+*/
+
+class floorData(name: String?, height:Int)
